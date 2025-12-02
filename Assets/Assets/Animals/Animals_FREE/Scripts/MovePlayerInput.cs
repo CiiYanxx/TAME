0cfdd1458604f0d2dd4e4d3368f3bfe59422beaa
CiiYanxx <bsemc.florendovictorg@gmail.com>
2025-12-03 06:33:36 +0800

    Adjustment

--- Assets/Assets/Animals/Animals_FREE/Scripts/MovePlayerInput.cs
+++ Assets/Assets/Animals/Animals_FREE/Scripts/MovePlayerInput.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace ithappy.Animals_FREE
 {
@@ -6,31 +7,20 @@ namespace ithappy.Animals_FREE
     public class MovePlayerInput : MonoBehaviour
     {
         [Header("Character")]
-        [SerializeField]
-        private string m_HorizontalAxis = "Horizontal";
-        [SerializeField]
-        private string m_VerticalAxis = "Vertical";
-        [SerializeField]
-        private string m_JumpButton = "Jump";
-        [SerializeField]
-        private KeyCode m_RunKey = KeyCode.LeftShift;
+        public InputActionReference MoveAction;
+        public InputActionReference JumpAction;
+        public InputActionReference RunAction;
 
         [Header("Camera")]
-        [SerializeField]
-        private PlayerCamera m_Camera;
-        [SerializeField]
-        private string m_MouseX = "Mouse X";
-        [SerializeField]
-        private string m_MouseY = "Mouse Y";
-        [SerializeField]
-        private string m_MouseScroll = "Mouse ScrollWheel";
+        public PlayerCamera m_Camera;
+        public InputActionReference LookAction;
+        public InputActionReference ScrollAction;
 
         private CreatureMover m_Mover;
 
         private Vector2 m_Axis;
         private bool m_IsRun;
         private bool m_IsJump;
-
         private Vector3 m_Target;
         private Vector2 m_MouseDelta;
         private float m_Scroll;
@@ -40,39 +30,49 @@ namespace ithappy.Animals_FREE
             m_Mover = GetComponent<CreatureMover>();
         }
 
+        private void OnEnable()
+        {
+            MoveAction.action.Enable();
+            JumpAction.action.Enable();
+            RunAction.action.Enable();
+            LookAction.action.Enable();
+            ScrollAction.action.Enable();
+        }
+
+        private void OnDisable()
+        {
+            MoveAction.action.Disable();
+            JumpAction.action.Disable();
+            RunAction.action.Disable();
+            LookAction.action.Disable();
+            ScrollAction.action.Disable();
+        }
+
         private void Update()
         {
             GatherInput();
             SetInput();
         }
 
-        public void GatherInput()
+        private void GatherInput()
         {
-            m_Axis = new Vector2(Input.GetAxis(m_HorizontalAxis), Input.GetAxis(m_VerticalAxis));
-            m_IsRun = Input.GetKey(m_RunKey);
-            m_IsJump = Input.GetButton(m_JumpButton);
+            m_Axis = MoveAction.action.ReadValue<Vector2>();
+            m_IsJump = JumpAction.action.IsPressed();
+            m_IsRun = RunAction.action.IsPressed();
 
             m_Target = (m_Camera == null) ? Vector3.zero : m_Camera.Target;
-            m_MouseDelta = new Vector2(Input.GetAxis(m_MouseX), Input.GetAxis(m_MouseY));
-            m_Scroll = Input.GetAxis(m_MouseScroll);
+
+            m_MouseDelta = LookAction.action.ReadValue<Vector2>();
+            m_Scroll = ScrollAction.action.ReadValue<float>();
         }
 
-        public void BindMover(CreatureMover mover)
-        {
-            m_Mover = mover;
-        }
-
-        public void SetInput()
+        private void SetInput()
         {
             if (m_Mover != null)
-            {
                 m_Mover.SetInput(in m_Axis, in m_Target, in m_IsRun, m_IsJump);
-            }
 
             if (m_Camera != null)
-            {
                 m_Camera.SetInput(in m_MouseDelta, m_Scroll);
-            }
         }
     }
 }
